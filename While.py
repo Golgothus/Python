0df0e02ab0049d1017515410f3009f4389160280
Golgothus <zachk0804@gmail.com>
2017-01-18 18:31:36 -0600

    Updating shenanigans.

---
+++ While.py
@@ -0,0 +1,19 @@
+# Beginning of the end.
+# I'm embariking on my journey to learn Python
+# These comments will always be in the beginning of my programs
+# Enjoy, Golgothus
+# Ch. 2 While Statements
+
+spam = 0
+if spam < 5:
+    print('Hello, world.')
+    spam = spam + 1
+
+print('')
+print('-----')
+print('')
+
+spam = 0
+while spam < 5:
+    print('Hello, world ' + str(spam) + ' .')
+    spam = spam + 1
---
+++ yourName.py
@@ -0,0 +1,25 @@
+# Beginning of the end.
+# I'm embariking on my journey to learn Python
+# These comments will always be in the beginning of my programs
+# Enjoy, Golgothus
+# Ch. 2 While Statements
+
+name = ''
+while name.lower() != 'your name':
+# Above statment makes the users input and forces it to all lower case
+    print('Please type \'your name\'.')
+    name = input()
+
+print('Thank you.')
+
+print('')
+print('-----')
+print('')
+# Break Statements
+
+while True:
+    print('Please type your name.')
+    name = input().lower()
+    if name == 'your name':
+        break
+print('Thanks!')

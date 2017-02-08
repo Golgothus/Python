145240f5e54518ea9ac95c0e154a21edc7778a73
Golgothus <zachk0804@gmail.com>
2017-02-08 10:44:00 -0600

    One new file and one modified file.

---
+++ schfiftyFive.py
@@ -0,0 +1,9 @@
+# Beginning of the end.
+# I'm embariking on my journey to learn Python
+# These comments will always be in the beginning of my programs
+# Enjoy, Golgothus
+# Ch. 2 SchfiftyFive
+
+print('My name is')
+for i in range(23):
+    print('Schfifty five! (' + str(i +1) + ').')
--- swardFish.py
+++ swardFish.py
@@ -3,3 +3,14 @@
 # These comments will always be in the beginning of my programs
 # Enjoy, Golgothus
 # Ch. 2 Continue
+
+while True:
+    print('Who are you?')
+    name = input()
+    if name != 'Joe':
+        continue
+    print('Hello, Joe. What is the password? (it is a fish.)')
+    password = input()
+    if password == 'swardFish':
+        break
+print('Access Granted. Welcome, Joe.')

0a68218e5926e76ce480d59775824ab4c1700479
Golgothus <zachk0804@gmail.com>
2017-02-23 22:18:27 -0600

    EquivWhile added.

--- equivWhile.py
+++ equivWhile.py
@@ -2,8 +2,11 @@
 # I'm embariking on my journey to learn Python
 # These comments will always be in the beginning of my programs
 # Enjoy, Golgothus
-# Ch. 2 SchfiftyFive
+# Ch. 2 Equivalent While Loops pg 55
 
-print('My name is')
-for i in range(23):
-    print('Schfifty five! (' + str(i +1) + ').')
+print ('My name is: ')
+name = input()
+n = 0
+while n < 15:
+    print('Hello, ' + name + '. This is sentencer number (' + str(n + 1) + ').')
+    n = n + 1

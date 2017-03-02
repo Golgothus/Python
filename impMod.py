7eee59d75e03036993a8f815ad7c8a00570ed803
Golgothus <zachk0804@gmail.com>
2017-03-02 09:24:37 -0600

    Added impMod.

---
+++ impMod.py
@@ -0,0 +1,10 @@
+# Beginning of the end.
+# I'm embariking on my journey to learn Python
+# These comments will always be in the beginning of my programs
+# Enjoy, Golgothus
+# Ch. 2 Equivalent While Loops pg 55
+
+import random
+
+for i in range(100):
+    print("The number for " + str(i + 1) + " is " + str(random.randint(1,100))+ ".")

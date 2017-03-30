f309e2bc66ac6a07dd784f72cf0b42e2b2d8b88f
golgothus <zachk0804@gmail.com>
2017-03-29 22:24:16 -0500

    I think I excluded OS from the directory? Unsure.

---
+++ projects/join.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Beginning of the end.
+# I'm embarking on my journey to learn Python
+# These comments will always be in the beginning of my programs
+# Enjoy, Golgothus
+# https://github.com/Golgothus
+
+import os
+os.chdir('//home/golgothus/Python/')
+print(os.getcwd())
+"""
+# os.path.join('~','Python','projects')
+
+
+myFiles = ['ex1.py', 'ex2.py', 'ex3.py']
+for fileName in myFiles:
+    print(os.path.join('//home/golgothus/Python/', fileName))
+
+os.makedirs('test')
+"""

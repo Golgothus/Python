e99d6ed96041155d49647af521422efcd6776882
golgothus <zachk0804@gmail.com>
2017-05-16 18:33:28 -0500

    New folder.

---
+++ Project/Test.py
@@ -0,0 +1,5 @@
+# Beginning of the end.
+# I'm embarking on my journey to learn Python
+# These comments will always be in the beginning of my programs
+# Enjoy, Golgothus
+# https://github.com/Golgothus

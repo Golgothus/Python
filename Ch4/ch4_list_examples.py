ed7be323d8fcf8e383863b7b429fb9a6cd9c12c7
golgothus <6819665+Golgothus@users.noreply.github.com>
2023-06-15 22:10:58 -0500

    Added a few more exercises, and modified list examples.py

--- Ch4/ch4_list_examples.py
+++ Ch4/ch4_list_examples.py
@@ -1,16 +1,19 @@
 #! /usr/bin/python
 
 
-def regularList():
+languages = ['Powershell','Python','HTML','CSS','jQuery']
+
+
+def regularList(list):
     # The following is an example of a regular list
-    languages = ['Powershell','Python','HTML','CSS','jQuery']
-    listLen = len(languages)
+    #languages = ['Powershell','Python','HTML','CSS','jQuery']
+    return len(list)
 
     
     print('List of languages I\'m slightly familiar with, are as follows:')
 
 
-    for l in languages:
+    for l in list:
         print(f'- {l}')
 
 
@@ -28,18 +31,62 @@ def multiList():
     print(multiValueList[1][3]) # prints the item from list 1, with index 3
 
 
-def negativeList():
-    languages = ['Powershell','Python','HTML','CSS','jQuery']
+def negativeList(list):
+    print(list[-4])
+    print(list[-3])
+    print(list[-2])
+    print(list[-1])
+    print(list[0])
 
-    print(languages[-4])
-    print(languages[-3])
-    print(languages[-2])
-    print(languages[-1])
-    print(languages[0])
+
+def sliceList(list):
+    print(list)
+    # you can additionally leave out the second integer
+    # which will print the starting index:length_of_the_list
+    print(f'Printing slice [3:], {list[3:]}')
+    print(f'Printing slice [1:], {list[1:]}')
+    print(f'Printing slice [0:], {list[0:]}')
+    print(f'Printing slice [:3], {list[:3]}')
+
+
+def getListLen(list):
+    return len(list)
+
+
+def changeList(list):
+    print(list)
+    list[1] = 'special'
+    print (list)
+
+
+def catList(list):
+    newList = ['test1','test2','test3']
+    newList = newList + list
+    print(newList)
+    newList = newList * 3
+    return newList
+
+
+def remList(list):
+    print(list)
+    del list[1]
+    print(list)
+
 
 print('Running regularList()')
-regularList()
+regularList(languages)
 print('Running multiList()')
 multiList()
 print('Running negativeList()')
-negativeList()
+negativeList(languages)
+print('Running sliceList():')
+sliceList(languages)
+print('Running getListLen():')
+print(getListLen(languages))
+print('Running changeList():')
+changeList(languages)
+print('Running catList():')
+catList(languages)
+print('Running remList():')
+remList(languages)
+regularList(languages)
---
+++ Ch4/inList.py
@@ -0,0 +1,5 @@
+#! /usr/bin/python
+
+languages = ['Powershell','Python','HTML','CSS','jQuery']
+
+print('howdy' in  languages)

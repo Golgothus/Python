ed69bce67d7eb621f8378e51800f4d25d88610fc
golgothus <6819665+Golgothus@users.noreply.github.com>
2023-08-30 22:12:19 -0500

    Successfully extracted phone numbers from clipboard text. Next up, emails.

--- Ch7/phone_extractor.py
+++ Ch7/phone_extractor.py
@@ -11,37 +11,57 @@
 
 import pyperclip, re
 
-def rex_phone_number(item):
-    phone_regex = re.compile(r'''(
+
+def rex_phone_number():
+    phone_regex = re.compile(
+        r"""(
         (\d{3}|\(\d{3}\))?              # area code
         (\s|-|\.)?                      # separator
-        \d{3}                           # middle 3 of number
+        (\d{3})                           # middle 3 of number
         (\s|-|\.)                       # separator
-        \d{4}                           # last 4 of number
+        (\d{4})                           # last 4 of number
         (\s*(ext|x|ext.)\s*\d{2,5})?    # extension, if present
-        )''',re.VERBOSE)
+        )""",
+        re.VERBOSE,
+    )
+    return phone_regex
 
-    print(item)
 
-    mo = phone_regex.search(item)
-    return mo.groups()
-
-def rex_email(item):
-    email_regex = re.compile(r'''(
+def rex_email():
+    email_regex = re.compile(
+        r"""(
         [a-zA-Z0-9._%+-]+
         @
         [a-zA-Z0-9.-]+
         (\.[a-zA-Z]{2,63})
-        )''',re.VERBOSE)
+        )""",
+        re.VERBOSE,
+    )
+    return email_regex
 
-def clip_text():
+
+def clip_text(copied_text):
     matches = []
-    #text = str(pyperclip.paste())
 
-    print(rex_phone_number("999-999-9999"))
+    for groups in rex_phone_number().findall(copied_text):
+        if groups[1] != "":
+            phone = "-".join([groups[1], groups[3], groups[5]])
+        else:
+            continue
+        if groups[7] != "":
+            phone += " x" + groups[8]
+        matches.append(phone)
 
-    rex_email(text)
+    return matches
+
 
 # TODO: Copy results to the clipboard
 
-clip_text()
+# print(clip_text(pyperclip.paste()))
+# print(clip_text(pyperclip.paste()))
+phone_numbers = clip_text(pyperclip.paste())
+new_phone = ""
+for number in phone_numbers:
+    new_phone += number + "\n"
+
+print(new_phone)
